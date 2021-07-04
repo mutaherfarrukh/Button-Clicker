9bb4723e34f53f7d37ee449564e0e3e37088f50f
mutaherfarrukh <86249387+mutaherfarrukh@users.noreply.github.com>
2021-07-04 12:20:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function SignClick(){
+    alert("Ninja was liked")
+}
+
+function Logout(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+
+
+
+
+
